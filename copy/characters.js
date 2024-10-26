0c441e1eb07b25bc69f86d64eb72e27cdadb02a2
Amircka07 <gitahubamir@gmail.com>
2024-10-26 20:13:37 +0600

    Add files via upload

---
+++ copy/characters.js
@@ -0,0 +1,26 @@
+const charactersBlock = document.querySelector(".characters");
+
+async function fetchCharactersData() {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+
+    data.forEach((character) => {
+      const characterBlock = document.createElement("div");
+      characterBlock.setAttribute("class", "characterCard");
+      characterBlock.innerHTML = `
+                <h2 class="name">${character.name}</h2>
+                <div class="characterPhoto">
+                    <img src="${character.photo}">
+                </div>
+                <p class="bio">${character.bio}</p>
+            `;
+      charactersBlock.append(characterBlock);
+    });
+    console.log(data);
+  } catch (error) {
+    console.log("Fetch error:", error);
+  }
+}
+
+fetchCharactersData();
